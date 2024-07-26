cb3a1a53548e0332db93dc69db0acf19574c3aba
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 14:59:06 +0530

    add customer form developing

--- src/app/page/customer-manage/add-customer/add-customer.component.ts
+++ src/app/page/customer-manage/add-customer/add-customer.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-customer',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css'
 })
 export class AddCustomerComponent {
 
+  constructor(private http: HttpClient) { }
+
+  public customerObj = {
+    name: "",
+    city: "",
+    contact: ""
+  }
+
+  addCustomer() {
+    this.http.post("http://localhost:8080/customer-controller/add-customer", this.customerObj).subscribe(
+      (data) => {
+        Swal.fire({
+          title: "Employee Added !",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+
+        console.log("Add Customer");
+
+      }
+    )
+  }
 }

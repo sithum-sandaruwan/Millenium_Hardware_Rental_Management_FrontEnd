53c8257c3955ecdfd3be47ba824148dd75311d86
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 14:06:21 +0530

    First Commit

---
+++ src/app/page/customer-manage/customer-manage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-manage',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-manage.component.html',
+  styleUrl: './customer-manage.component.css'
+})
+export class CustomerManageComponent {
+
+}

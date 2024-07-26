e7fbc40f5f981fcce833ae9e4acd776c426ca20f
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 15:31:48 +0530

    add customer developing

--- src/app/page/customer-manage/add-customer/add-customer.component.ts
+++ src/app/page/customer-manage/add-customer/add-customer.component.ts
@@ -1,19 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-customer',
   standalone: true,
-  imports: [FormsModule, HttpClientModule, CommonModule],
+  imports: [FormsModule, HttpClientModule,CommonModule],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.css'
 })
-export class AddCustomerComponent {
 
-  constructor(private http: HttpClient) { }
+export class AddCustomerComponent {
 
   public customerObj = {
     name: "",
@@ -21,18 +19,14 @@ export class AddCustomerComponent {
     contact: ""
   }
 
+  constructor(private http: HttpClient) { }
+
   addCustomer() {
-    this.http.post("http://localhost:8080/customer-controller/add-customer", this.customerObj).subscribe(
+    this.http.post("http://localhost:8080/customer-controller/add-customer",this.customerObj).subscribe(
       (data) => {
-        Swal.fire({
-          title: "Employee Added !",
-          text: "You clicked the button!",
-          icon: "success"
-        });
-
-        console.log("Add Customer");
-
+        console.log("added")
       }
     )
-  }
+   }
+
 }

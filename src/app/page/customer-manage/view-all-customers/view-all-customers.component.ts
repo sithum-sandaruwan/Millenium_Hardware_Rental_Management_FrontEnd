6742facbd52842a36f067ecfdde458093a5bee88
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 18:00:06 +0530

    view all edited

--- src/app/page/customer-manage/view-all-customers/view-all-customers.component.ts
+++ src/app/page/customer-manage/view-all-customers/view-all-customers.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all-customers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './view-all-customers.component.html',
   styleUrl: './view-all-customers.component.css'
 })
 export class ViewAllCustomersComponent {
 
+  public customerList: any;
+
+  constructor(private http: HttpClient) {
+    this.loadCustomerTable();
+  }
+
+  loadCustomerTable() {
+      this.http.get("http://localhost:8080/customer-controller/get-all-cust").subscribe(
+        res =>{
+          this.customerList=res;
+          console.log(this.customerList);
+          
+        }
+      )
+  }
 }

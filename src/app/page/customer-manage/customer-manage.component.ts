8219b6b4b86694177ab2c870cdbe6829df5837c2
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 14:29:48 +0530

    add customer page created

--- src/app/page/customer-manage/customer-manage.component.ts
+++ src/app/page/customer-manage/customer-manage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
 
 @Component({
   selector: 'app-customer-manage',
   standalone: true,
-  imports: [],
+  imports: [AddCustomerComponent],
   templateUrl: './customer-manage.component.html',
   styleUrl: './customer-manage.component.css'
 })

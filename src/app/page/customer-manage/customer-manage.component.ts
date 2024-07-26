d30953592c961020b0d3652303c6a68af2ec947b
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-26 16:54:37 +0530

    delete customer component created

--- src/app/page/customer-manage/customer-manage.component.ts
+++ src/app/page/customer-manage/customer-manage.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { ViewAllCustomersComponent } from './view-all-customers/view-all-customers.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
 
 @Component({
   selector: 'app-customer-manage',
   standalone: true,
-  imports: [AddCustomerComponent],
+  imports: [AddCustomerComponent,ViewAllCustomersComponent,DeleteCustomerComponent,UpdateCustomerComponent],
   templateUrl: './customer-manage.component.html',
   styleUrl: './customer-manage.component.css'
 })
